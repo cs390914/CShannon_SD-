7b687e9ab853294a139669a42212c966034c0b85
cs390914 <cs390914@COB-CLAB66>
2019-09-11 11:39:39 -0400

    intial work in class

---
+++ CShannon_SD!/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CShannon_SD_.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public ICollection<Order> orders { get; set; }
+    }
+}
---
+++ CShannon_SD!/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CShannon_SD_.Models
+{
+    public class Order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual Customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+    }
+}
